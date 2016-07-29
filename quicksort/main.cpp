6075ed7c6225ece867934f73e5bf42a532ee628a
simplydesigner <simplydesigner@ya.ru>
2016-07-29 10:40:32 +0300

    Initial Commit

---
+++ quicksort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  quicksort
+//
+//  Created by simplydesigner on 7/29/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
